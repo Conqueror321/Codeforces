4585395b5a2e6a7a45ac1aa78e27fadee5f31bab
YutaOkkotsu321 <dastan.k06g@gmail.com>
2026-05-16 00:27:40 +0500

    first commit

---
+++ 900/template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define pb push_back
+#define vt vector
+#define REP(n) for(int i = 0; i < n; i++)
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+        
+    }
+}
