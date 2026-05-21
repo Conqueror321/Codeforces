318ab43abb7319e1c1208bbcf0a3957f148df9aa
YutaOkkotsu321 <dastan.k06g@gmail.com>
2026-05-22 02:39:14 +0500

    fixed: original titles are recovered

--- 900/1807D.cpp
+++ 900/1807D.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #define ll long long
 using namespace std;
 
@@ -17,7 +18,7 @@ int main() {
         }
         vector<ll> pre(n + 1);
         pre[0] = 0;
-        for(ll i = 1; i < n; i++) {
+        for(ll i = 1; i <= n; i++) {
             pre[i] = num[i - 1] + pre[i - 1];
         }
 
@@ -25,7 +26,7 @@ int main() {
             ll l, r, k;
             cin >> l >> r >> k;
             ll temp = (r - l + 1) * k;
-            if((total - pre[r] - pre[l - 1] + temp) % 2) cout << "YES" << '\n';
+            if((total - (pre[r] - pre[l - 1]) + temp) % 2) cout << "YES" << '\n';
             else cout << "NO" << '\n';
             
         }
---
+++ 900/1971C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if(a > b) swap(a, b);
+
+        bool cInside = a < c && c < b;
+        bool dInside = a < d && d < b;
+
+        cout << (cInside != dInside ? "YES" : "NO") << '\n';
+    }
+}
